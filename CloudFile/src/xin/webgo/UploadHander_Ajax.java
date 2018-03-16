6386f4643fa91b52ce4e184d490d890a631d9f3e
Administrator <Administrator@LMEN822G32ZJ43O>
2018-03-16 10:07:24 +0800

    去除冗余文件

--- CloudFile/src/xin/webgo/UploadHander_Ajax.java
+++ CloudFile/src/xin/webgo/UploadHander_Ajax.java
@@ -58,14 +58,13 @@ public class UploadHander_Ajax extends HttpServlet {
 		      //3、判断提交上来的数据是否是上传表单的数据
 		      if(!ServletFileUpload.isMultipartContent(request)){
 		       //按照传统方式获取数据
-		    	  System.out.print("普通表单数据");
 		       return;
 		      }
 		    
 		      //设置上传单个文件的大小的最大值，目前是设置为1024*1024字节，也就是1MB
 		      upload.setFileSizeMax(10*1024*1024);
 		      //设置上传文件总量的最大值，最大值=同时上传的多个文件的大小的最大值的和，目前设置为10MB
-		      upload.setSizeMax(50*1024*1024);
+		      upload.setSizeMax(10*1024*1024);
 		      //4、使用ServletFileUpload解析器解析上传数据，解析结果返回的是一个List<FileItem>集合，每一个FileItem对应一个Form表单的输入项
 		      List<FileItem> list;
 		      HashMap<String,String> map = new HashMap<String,String>();
@@ -81,7 +80,6 @@ public class UploadHander_Ajax extends HttpServlet {
 		    		  System.out.print(name);
 		    	  }
 		    	  else{//如果fileitem中封装的是上传文件
-		    		  System.out.print("发现文件");
 		    		  in = item.getInputStream();
 		    		  String tempSavePath = makePath(uploadTemp,map.get("uuid"),map.get("fileName"),map.get("currChunk"));
 		    		  FileOutputStream out = new FileOutputStream(tempSavePath);
@@ -96,61 +94,12 @@ public class UploadHander_Ajax extends HttpServlet {
 					 in.close();
 					 out.close(); 
 					 item.delete();
-		    		  //continue;
-		        //得到上传的文件名称，
-		       /* fileName = item.getName();
-		        System.out.println(fileName);
-		        if(fileName==null || fileName.trim().equals("")){
-		         continue;*/
 		    	  }		    	 
 		      } 
 		      }catch (FileUploadException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
 				}
-		         
-		        //注意：不同的浏览器提交的文件名是不一样的，有些浏览器提交上来的文件名是带有路径的，如： c:\a\b\1.txt，而有些只是单纯的文件名，如：1.txt
-		        //处理获取到的上传文件的文件名的路径部分，只保留文件名部分
-		       /* fileName = fileName.substring(fileName.lastIndexOf("\\")+1);
-		        //得到上传文件的扩展名
-		        String fileExtName = fileName.substring(fileName.lastIndexOf(".")+1);
-		        //如果需要限制上传的文件类型，那么可以通过文件的扩展名来判断上传的文件类型是否合法
-		        System.out.println("上传的文件的扩展名是："+fileExtName);
-		        //获取item中的上传文件的输入流
-		        InputStream in = item.getInputStream();
-		        //得到文件保存的名称
-		        String saveFilename = makeFileName(fileName);
-		        //得到文件的保存目录
-		        String hashPath = makePath(saveFilename, savePath);
-		        realSavePath = hashPath + "\\" + saveFilename;
-		        //创建一个文件输出流
-		        FileOutputStream out = new FileOutputStream(realSavePath);
-		       //创建一个缓冲区
-		        byte buffer[] = new byte[1024];
-		        //判断输入流中的数据是否已经读完的标识        
-		        int len = 0;
-		        //循环将输入流读入到缓冲区当中，(len=in.read(buffer))>0就表示in里面还有数据
-		        while((len=in.read(buffer))>0){
-		         //使用FileOutputStream输出流将缓冲区的数据写入到指定的目录(savePath + "\\" + filename)当中
-		         out.write(buffer, 0, len);
-		        }
-		        //关闭输入流
-		        in.close();
-		        //关闭输出流
-		        out.close();        //删除处理文件上传时生成的临时文件        //item.delete(); 
-		       }*/
-		//获取分块信息
-		/*String uuid = request.getParameter("uuid");
-		String fileName = request.getParameter("fileName");
-		String currChunk = request.getParameter("currChunk");
-		String totalChunk = request.getParameter("totalChunk");	
-		String data = request.getParameter("data");
-		String path = makePath(tempPath,uuid,fileName,currChunk);
-		System.out.print(path);
-		File tempFile = new File(path);
-		DataOutputStream out = new DataOutputStream(new BufferedOutputStream(new FileOutputStream(tempFile)));
-		out.writeBytes(data);
-		out.close();*/
 	}
 
 	/**
